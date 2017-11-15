232352ecc4056e55265e665a33651188f8eafb23
Mah <Mah@sd>
2017-11-15 06:16:24 +0330

    g

---
+++ cordova_ios_release.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Created by App Builder v2017.93
+# More information at https://www.davidesperalta.com/
+
+# Remember to set exec permissions to this script with "chmod +x cordova_ios_release.sh"
+
+# Add the iOS platform for our app
+cordova platform add ios
+
+# Add the Whitelist plugin for our app
+cordova plugin add cordova-plugin-whitelist
+
+# Add the Location plugin for our app
+cordova plugin add cordova-plugin-geolocation
+
+# Add the Vibration plugin for our app
+cordova plugin add cordova-plugin-vibration
+
+# Add the In App Browser plugin for our app
+cordova plugin add cordova-plugin-inappbrowser
+
+# Build our app for the iOS platform
+cordova build ios --release
+
+# Pause the script execution, so we can view the script results
+READ -p "Paused..."
